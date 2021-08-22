0642f5e5a06978dcc5f592cf4c0505c512aa0be8
W-yt <lalalu9514@gmail.com>
2021-08-22 15:37:54 +0800

    finish the python-project1(python basic without class)

--- python/pythonProject1/main.py
+++ python/pythonProject1/main.py
@@ -2,5 +2,24 @@
 # for i in range(10):
 #     print(i)
 
+# pycharm Fast Typesetting —— ctrl+alt+l
 
+# #input
+# name = input("tel me your name: ")
+# print("Hello, "+name+"!")
 
+# #import the module
+# import pizza
+# pizza.make_pizza(16, 'mushrooms', 'green peppers', 'extra cheese')
+
+# #import the functions from module
+# from pizza import make_pizza
+# make_pizza(16, 'mushrooms', 'green peppers')
+
+# #import function from module and rename
+# from pizza import make_pizza as mk
+# mk(12, "mushrooms")
+
+#use as rename the module
+import pizza as p
+p.make_pizza(9, 'cheess')
