b9407bc545eeba058168f00932b5843d5245fc14
W-yt <lalalu9514@gmail.com>
2021-09-01 12:11:46 +0800

    finish python-project8 about basic visualization and randomWalk picture

--- python/pythonProject8/rw_visual.py
+++ python/pythonProject8/rw_visual.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # 设置绘图窗口尺寸
+    plt.figure(dpi = 128, figsize = (10,6))
+
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolors = 'none', s = 10)
+    plt.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolors = 'none', s = 1)
 
     # 突出起点和终点
-    plt.scatter(0, 0, c = 'green', edgecolors = 'none', s = 50)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolors = 'none', s = 50)
+    plt.scatter(0, 0, c = 'green', edgecolors = 'none', s = 10)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolors = 'none', s = 10)
 
     # 隐藏坐标轴
     plt.gca().get_xaxis().set_visible(False)
